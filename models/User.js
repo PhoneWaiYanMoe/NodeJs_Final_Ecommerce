62a70e34f0892b301d3acafdf2677e2058e99949
Phone Wai Yan Moe <142496995+PhoneWaiYanMoe@users.noreply.github.com>
2025-05-07 03:02:08 +0700

    Update User.js for multiple addresses

--- models/User.js
+++ models/User.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String }, // Optional initially
+  password: { type: String },
   name: { type: String, required: true },
-  role: { type: String, default: 'user' }, // user or admin
+  role: { type: String, default: 'user' },
   shippingAddress: {
     street: { type: String, required: true },
     city: { type: String, required: true },
@@ -12,6 +12,13 @@ const userSchema = new mongoose.Schema({
     zip: { type: String, required: true },
     country: { type: String, required: true },
   },
+  shippingAddressCollection: [{
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zip: { type: String, required: true },
+    country: { type: String, required: true },
+  }],
   createdAt: { type: Date, default: Date.now },
 });
 
