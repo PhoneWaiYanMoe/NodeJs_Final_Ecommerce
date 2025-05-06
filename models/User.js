34c13f6b01463f5aff6036b5c53cf4ab65e58d12
Phone Wai Yan Moe <142496995+PhoneWaiYanMoe@users.noreply.github.com>
2025-05-06 14:55:08 +0700

    Update User for address attr.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,17 @@
-// AccountManagerService/models/User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String },
-  name: { type: String },
+  password: { type: String }, // Optional initially
+  name: { type: String, required: true },
   role: { type: String, default: 'user' }, // user or admin
+  shippingAddress: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zip: { type: String, required: true },
+    country: { type: String, required: true },
+  },
   createdAt: { type: Date, default: Date.now },
 });
 
