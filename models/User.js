54b450388f2339ce5143f9b4fce276f021a2ceef
Juuzo26 <114993656+Juuzo26@users.noreply.github.com>
2025-05-16 19:44:58 +0700

    Update User.js with point attr

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String }, // Optional initially
   name: { type: String, required: true },
+  points: { type: Number, default: 0 }, // Adding points field
   role: { type: String, default: 'user' }, // user or admin
   shippingAddress: {
     street: { type: String, required: true },
