7b97bdf6bef7a7f66fbc3dfc01c19a8a091b3dc4
Phone Wai Yan Moe <142496995+PhoneWaiYanMoe@users.noreply.github.com>
2025-05-16 21:50:57 +0700

    Update CartItem.js for variantName

--- models/CartItem.js
+++ models/CartItem.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -5,6 +6,7 @@ const cartItemSchema = new Schema({
   userId: { type: String, default: null }, // For logged-in users
   sessionId: { type: String, default: null }, // For guest users
   productId: { type: Schema.Types.ObjectId, required: true }, // MongoDB ObjectID
+  variantName: { type: String, required: true }, // Changed default: null to required: true
   quantity: { type: Number, required: true, default: 1 },
   price: { type: Number, required: true },
 });
