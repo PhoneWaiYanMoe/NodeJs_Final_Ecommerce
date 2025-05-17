ed3eeb328c7ba7782e5716f9dea839d3d74869a6
Juuzo26 <114993656+Juuzo26@users.noreply.github.com>
2025-05-17 11:15:53 +0700

    app.js fix render

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(cors({
         }
     },
     credentials: true,
-    methods: ['GET', POST, 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
